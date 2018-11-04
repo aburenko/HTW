06a0c8d394b04ac979ee81cddcadf348b851ce5e
Burenko Anton <burenkoanton@gmail.com>
2018-11-04 14:37:17 +0100

    garbage cleared & commented

--- src/TCPClientAlice.java
+++ src/TCPClientAlice.java
@@ -3,25 +3,22 @@ import java.net.*;
 class TCPClientAlice { 
 
     public static void main(String argv[]) throws Exception 
-    { 
-
-        BufferedReader inFromUser = 
-          new BufferedReader(new InputStreamReader(System.in)); 
-
+    {
+        // create new client socket
         Socket clientSocket = new Socket("localhost", 6789); 
-
+        // set a output stream for client
         DataOutputStream outToServer = 
           new DataOutputStream(clientSocket.getOutputStream());
-
+        // get file to read from it
 		FileInputStream fileIn = new FileInputStream("alice.txt");
-	
+	    // arrange new buffer for sending a data from file
 		byte[] buffer = new byte[1024];
-	
+	    // check if there are available bytes to send
 		while(fileIn.available() > 0) {
+		    // send bytes from buffer from 0...number of bytes read
 	    	outToServer.write(buffer, 0, fileIn.read(buffer)); 
 		}
-
-		clientSocket.close(); 
-                   
+		// close socket when all bytes are sent
+		clientSocket.close();
     } 
 } 
