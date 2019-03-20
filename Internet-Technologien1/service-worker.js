35d9c8c7aac33b08932e3d69de09de9e4db31600
Burenko Anton <burenkoanton@gmail.com>
2019-03-20 18:34:45 +0100

    init

---
+++ Internet-Technologien1/service-worker.js
@@ -0,0 +1,21 @@
+var CACHE_NAME = 'my-site-cache-v1';
+var urlsToCache = [
+  '/',
+  '/styles/main.css',
+  '/scripts/main.js',
+  '/images/smiley.svg'
+];
+
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  console.log('Finally active. Ready to start serving content!');
+});
