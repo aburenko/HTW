b88d9298cbc47408b5ec9af4374415d9edd566ef
Burenko Anton <burenkoanton@gmail.com>
2019-03-30 00:19:38 +0100

    add json modules and randomizing logic compitable with it

--- Internet-Technologien1/scripts/main.js
+++ Internet-Technologien1/scripts/main.js
@@ -1,6 +1,54 @@
 "use strict";
-drawClefKey("treble", "c/4");
 
+var requestURL = { 
+	"treble":"/modules/treble.json",
+	"bass":"/modules/bass.json",
+	"alto":"/modules/alto.json",
+}
+
+var choosenTreble = "treble";
+var loadedNotes;
+var restNotes;
+
+loadModule(requestURL[choosenTreble]);
+
+
+function initModule(xhttp) {
+	// init gotten module notes to var(arr)
+	loadedNotes = JSON.parse(xhttp.responseText);
+	// init or reinit array to mark already shown notes
+	// any clef has 15 notes
+	restNotes = Array.from(Array(15).keys());
+}
+
+function nextNote() {
+	// pick a random number between 0 and the restNotes length
+	var randNum = Math.floor(Math.random() * restNotes.length);
+	console.log("randNum: " + randNum);
+	// remove that index number from the array
+	var roll = restNotes.splice(randNum, 1);
+	// clear box and draw next
+	clearBox("note");
+	console.log("draw: " + choosenTreble + " " + loadedNotes[roll]);
+	drawClefKey(choosenTreble, loadedNotes[roll]);
+	// check if all notes are showed
+	if(restNotes.length == 0)
+		document.getElementById("note").innerHTML = "DONE!!!";
+}
+
+function loadModule(moduleURL) { 
+	// loads module from module URL with AJAX 
+	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+			// call startModule after recieve
+			initModule(this);
+		}
+	};
+	// do asyc GET
+	xhttp.open("GET", moduleURL, true);
+	xhttp.send();
+}
 
 function drawClefKey(clefToDraw, keyToDraw) {
 	var VF = Vex.Flow;
@@ -8,7 +56,7 @@ function drawClefKey(clefToDraw, keyToDraw) {
 	var div = document.getElementById("note")
 	var renderer = new VF.Renderer(div, VF.Renderer.Backends.SVG);
 	// Size our svg:
-	// TODO: ... if render for mobile change size
+	// TODO: ... if render for mobile -> change size
 	renderer.resize(250, 250);
 	// And get a drawing context:
 	var context = renderer.getContext();
@@ -34,5 +82,6 @@ function drawClefKey(clefToDraw, keyToDraw) {
 
 function clearBox(elementID)
 {
+	// clears given box with elementID
     document.getElementById(elementID).innerHTML = "";
 }
