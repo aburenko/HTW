456e01c8c5c29f6fdfb962f918c63abecf9c858a
Burenko <burenkoanton@gmail.com>
2020-06-11 22:39:13 +0200

    final final final

--- IS/beleg/s76905.c
+++ IS/beleg/s76905.c
@@ -262,7 +262,6 @@ int main(void)
   // fwrite(decryptedtext, sizeof(unsigned char), decryptedtext_len, cipherFile);
   // fclose(cipherFile);
 
-  getKeys("s76905-source-key-corrupt.bin", key, iv, 32);
   unsigned char *md4Hash = md4_hash(decryptedtext);
   /* concatenate decrypted text with hash */
   int dest_size = size + 16;
