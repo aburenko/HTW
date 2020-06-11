8bec27ead7785c4a37ee2fcb675d2c5c4c174f27
Burenko <burenkoanton@gmail.com>
2020-06-11 21:59:34 +0200

    uhu 3 and 4

--- IS/beleg/s76905.c
+++ IS/beleg/s76905.c
@@ -23,52 +23,52 @@ void err_exit(void)
   exit(EXIT_FAILURE);
 }
 
-// static char *pt(unsigned char *md)
-// {
-//   int i;
-//   static char buf[16];
-//   for (i = 0; i < 16; i++)
-//   {
-//     // write as hexadecimal to buf
-//     sprintf(&(buf[i * 2]), "%02x", md[i]);
-//   }
-//   return (buf);
-// }
+static char *pt(unsigned char *md)
+{
+  int i;
+  static char buf[16];
+  for (i = 0; i < 16; i++)
+  {
+    // write as hexadecimal to buf
+    sprintf(&(buf[i * 2]), "%02x", md[i]);
+  }
+  return (buf);
+}
 
-// unsigned char *md4_hash(unsigned char plaintext[])
-// {
-//   printf("creating md4 hash");
-//   FILE *fin;
-//   long filesize;
+unsigned char *md4_hash(unsigned char plaintext[])
+{
+  printf("creating md4 hash");
+  FILE *fin;
+  long filesize;
 
-//   EVP_MD_CTX c;
-//   unsigned char md = malloc(sizeof(unsigned char) * 16);
+  EVP_MD_CTX c;
+  unsigned char md = malloc(sizeof(unsigned char) * 16);
 
-//   ERR_load_crypto_strings();
-//   EVP_MD_CTX_init(&c);
+  ERR_load_crypto_strings();
+  EVP_MD_CTX_init(&c);
 
-//   if ((EVP_DigestInit(&c, EVP_md4())) == 0)
-//   {
-//     err_exit();
-//   }
-//   if ((EVP_DigestUpdate(&c, plaintext, filesize)) == 0)
-//   {
-//     err_exit();
-//   }
-//   if ((EVP_DigestFinal(&c, md, NULL)) == 0)
-//   {
-//     err_exit();
-//   }
+  if ((EVP_DigestInit(&c, EVP_md4())) == 0)
+  {
+    err_exit();
+  }
+  if ((EVP_DigestUpdate(&c, plaintext, filesize)) == 0)
+  {
+    err_exit();
+  }
+  if ((EVP_DigestFinal(&c, md, NULL)) == 0)
+  {
+    err_exit();
+  }
 
-//   // print as hexadecimal
-//   printf("hash was %s\n", pt(md));
+  // print as hexadecimal
+  printf("hash was %s\n", pt(md));
 
-//   // clean ups
-//   EVP_MD_CTX_cleanup(&c);
-//   ERR_free_strings();
+  // clean ups
+  EVP_MD_CTX_cleanup(&c);
+  ERR_free_strings();
 
-//   return md;
-// }
+  return md;
+}
 
 void clean_up()
 {
@@ -148,36 +148,59 @@ int brutforce_decrypt(unsigned char ciphertext[], int ciphertext_len, unsigned c
   return -1;
 }
 
-int main(void)
+int encrypt(unsigned char *plaintext, int plaintext_len, unsigned char *key,
+            unsigned char *iv, unsigned char *ciphertext)
 {
-  printf("start main\n");
+  EVP_CIPHER_CTX *ctx;
+
+  int len;
+
+  int ciphertext_len;
+
+  /* Create and initialise the context */
+  if (!(ctx = EVP_CIPHER_CTX_new()))
+    handleErrors();
+
+  if (1 != EVP_EncryptInit_ex(ctx, EVP_aes_192_ofb(), NULL, key, iv))
+    handleErrors();
+
+  if (1 != EVP_EncryptUpdate(ctx, ciphertext, &len, plaintext, plaintext_len))
+    handleErrors();
+  ciphertext_len = len;
+
+  if (1 != EVP_EncryptFinal_ex(ctx, ciphertext + len, &len))
+    handleErrors();
+  ciphertext_len += len;
+
+  EVP_CIPHER_CTX_free(ctx);
+
+  return ciphertext_len;
+}
+
+void getKeys(char *file, unsigned char *key,
+            unsigned char *iv, int sizeKey)
+{
   FILE *keyFile;
   size_t result = 0;
-
-  printf("reading keys out of a file\n");
-  keyFile = fopen("s76905-source-key-corrupt.bin", "rb");
+  printf("reading keys out of a %s\n", file);
+  keyFile = fopen(file, "rb");
   if (keyFile == NULL)
   {
     fputs("File error", stderr);
     exit(1);
   }
 
-  /* A 256 bit key */
-  unsigned char key[32];
-  /* A 128 bit IV */
-  unsigned char iv[16];
-
   // copy the file into the keys:
-  result += fread(key, 1, 32, keyFile);
+  result += fread(key, 1, sizeKey, keyFile);
   result += fread(iv, 1, 16, keyFile);
   fclose(keyFile);
-  if (result != 48)
+  if (result != sizeKey + 16)
   {
     fputs("Reading error", stderr);
     exit(2);
   }
   printf("key: ");
-  for (int i = 0; i < 32; i++)
+  for (int i = 0; i < sizeKey; i++)
   {
     printf("%02x", key[i]);
   }
@@ -186,6 +209,18 @@ int main(void)
   {
     printf("%02x", iv[i]);
   }
+}
+
+int main(void)
+{
+  printf("start main\n");
+
+  /* A 256 bit key */
+  unsigned char key[32];
+  /* A 128 bit IV */
+  unsigned char iv[16];
+  getKeys("s76905-source-key-corrupt.bin", key, iv, 32)
+
   printf("\nreading cipher text out of a file\n");
   // Reading size of file
   FILE *cipherFile = fopen("s76905-source-cipher.bin", "r+");
@@ -219,12 +254,34 @@ int main(void)
     printf("\nno match for %PDF found\n");
     exit(5);
   }
-  printf("\nsaving decrypted text\n");
-  cipherFile = fopen("output.pdf", "w+");
-  fwrite(decryptedtext, sizeof(unsigned char), decryptedtext_len, cipherFile);
-  fclose(cipherFile);
 
-  // unsigned char *md4Hash = md4_hash(decryptedtext);
+  /* save decrypted pdf file */
+  // printf("\nsaving decrypted text\n");
+  // cipherFile = fopen("output.pdf", "w+");
+  // fwrite(decryptedtext, sizeof(unsigned char), decryptedtext_len, cipherFile);
+  // fclose(cipherFile);
+
+  getKeys("s76905-source-key-corrupt.bin", key, iv, 32)
+  unsigned char *md4Hash = md4_hash(decryptedtext);
+  /* concatenate decrypted text with hash */
+  int dest_size = size + 16;
+  unsigned char hashedtext[dest_size];
+  unsigned char encryptedhashedtext[dest_size];
+  strcpy(hashedtext, decryptedtext);
+  strcat(hashedtext, md4Hash);
+  /* Encrypt the plaintext with hash */
+  unsigned char keyAes[24];
+  getKeys("s76905-dest-key.bin", key, iv, 24);
+  int encrypted_len = 0;
+  ciphertext_len = encrypt(hashedtext, dest_size, keyAes, iv,
+                           encryptedhashedtext);
+
+
+  /* save encrypted pdf with hash */
+  printf("\nsaving enrypted text + hash\n");
+  cipherFile = fopen("s76905-dest-cipher.bin", "w+");
+  fwrite(encryptedhashedtext, sizeof(unsigned char), encrypted_len, cipherFile);
+  fclose(cipherFile);
 
   printf("exit\n");
   return 0;
