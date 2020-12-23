254d4a43e3c396a78f1b158eb4798cc83c207ba9
Burenko <burenkoanton@gmail.com>
2020-12-23 15:52:26 +0200

    working on code generation

---
+++ Compiler_Interpreter/pl0Compiler/cmake-build-debug/CMakeFiles/pl0Compiler.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "C:/edu/HTW/Compiler_Interpreter/pl0Compiler/codegen.c" "C:/edu/HTW/Compiler_Interpreter/pl0Compiler/cmake-build-debug/CMakeFiles/pl0Compiler.dir/codegen.c.obj"
+  "C:/edu/HTW/Compiler_Interpreter/pl0Compiler/lex.c" "C:/edu/HTW/Compiler_Interpreter/pl0Compiler/cmake-build-debug/CMakeFiles/pl0Compiler.dir/lex.c.obj"
+  "C:/edu/HTW/Compiler_Interpreter/pl0Compiler/main.c" "C:/edu/HTW/Compiler_Interpreter/pl0Compiler/cmake-build-debug/CMakeFiles/pl0Compiler.dir/main.c.obj"
+  "C:/edu/HTW/Compiler_Interpreter/pl0Compiler/namelist.c" "C:/edu/HTW/Compiler_Interpreter/pl0Compiler/cmake-build-debug/CMakeFiles/pl0Compiler.dir/namelist.c.obj"
+  "C:/edu/HTW/Compiler_Interpreter/pl0Compiler/parser.c" "C:/edu/HTW/Compiler_Interpreter/pl0Compiler/cmake-build-debug/CMakeFiles/pl0Compiler.dir/parser.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Compiler_Interpreter/pl0Compiler/cmake-build-debug/CMakeFiles/pl0Compiler.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pl0Compiler.dir/codegen.c.obj"
+  "CMakeFiles/pl0Compiler.dir/lex.c.obj"
+  "CMakeFiles/pl0Compiler.dir/main.c.obj"
+  "CMakeFiles/pl0Compiler.dir/namelist.c.obj"
+  "CMakeFiles/pl0Compiler.dir/parser.c.obj"
+  "libpl0Compiler.dll.a"
+  "pl0Compiler.exe"
+  "pl0Compiler.exe.manifest"
+  "pl0Compiler.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/pl0Compiler.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
